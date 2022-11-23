9abb223dbb2c71f36c3816f0fdd2e5464e69c3d7
PkmRiRu <118901305+PkmRiRu@users.noreply.github.com>
2022-11-23 15:20:31 +0100

    Update moves.js

--- calc/data/moves.js
+++ calc/data/moves.js
@@ -182,7 +182,7 @@ var RBY = {
     Agility: { bp: 0, category: 'Status', type: 'Psychic' },
     'Confuse Ray': { bp: 0, category: 'Status', type: 'Ghost' },
     Confusion: { bp: 50, type: 'Psychic' },
-    Cut: { bp: 50, type: 'Normal' },
+    Cut: { bp: 70, type: 'Grass' },
     'Double Team': { bp: 0, category: 'Status', type: 'Normal' },
     'Drill Peck': { bp: 80, type: 'Flying' },
     Ember: { bp: 40, type: 'Fire' },
